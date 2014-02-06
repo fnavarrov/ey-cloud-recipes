584fc775bfdbc90fd14c876c03390da15325aa66
Felipe Navarro V <fnavarrov@gmail.com>
2014-02-06 00:04:38 -0300

    fixing node in sphinx

--- cookbooks/sphinx/recipes/default.rb
+++ cookbooks/sphinx/recipes/default.rb
@@ -34,8 +34,8 @@ if ! File.exists?("/data/#{appname}/current")
   Chef::Log.info "Sphinx was not configured because the app must be deployed first.  Please deploy it then re-run custom recipes."
 else
   if utility_name
-    sphinx_host = @node[:utility_instances].find {|u| u[:name] == utility_name }[:hostname]
-    if ['solo', 'app', 'app_master'].include?(@node[:instance_role])
+    sphinx_host = node[:utility_instances].find {|u| u[:name] == utility_name }[:hostname]
+    if ['solo', 'app', 'app_master'].include?(node[:instance_role])
       run_for_app(appname) do |app_name, data|
         ey_cloud_report "Sphinx" do
           message "configuring #{flavor}"
@@ -43,49 +43,49 @@ else
 
         directory "/data/#{app_name}/shared/config/sphinx" do
           recursive true
-          owner @node[:owner_name]
-          group @node[:owner_name]
+          owner node[:owner_name]
+          group node[:owner_name]
           mode 0755
         end
 
         template "/data/#{app_name}/shared/config/sphinx.yml" do
-          owner @node[:owner_name]
-          group @node[:owner_name]
+          owner node[:owner_name]
+          group node[:owner_name]
           mode 0644
           source "sphinx.yml.erb"
           variables({
             :app_name => app_name,
             :address => sphinx_host,
-            :user => @node[:owner_name],
+            :user => node[:owner_name],
             :mem_limit => '32M'
           })
         end
       end
     end
 
-    if @node[:name] == utility_name
+    if node[:name] == utility_name
       run_for_app(appname) do |app_name, data|
         ey_cloud_report "Sphinx" do
           message "configuring #{flavor}"
         end
 
         directory "/var/run/sphinx" do
-          owner @node[:owner_name]
-          group @node[:owner_name]
+          owner node[:owner_name]
+          group node[:owner_name]
           mode 0755
         end
 
         directory "/var/log/engineyard/sphinx/#{app_name}" do
           recursive true
-          owner @node[:owner_name]
-          group @node[:owner_name]
+          owner node[:owner_name]
+          group node[:owner_name]
           mode 0755
         end
 
         directory "/data/#{app_name}/shared/config/sphinx" do
           recursive true
-          owner @node[:owner_name]
-          group @node[:owner_name]
+          owner node[:owner_name]
+          group node[:owner_name]
           mode 0755
         end
 
@@ -100,25 +100,25 @@ else
 
         template "/etc/monit.d/sphinx.#{app_name}.monitrc" do
           source "sphinx.monitrc.erb"
-          owner @node[:owner_name]
-          group @node[:owner_name]
+          owner node[:owner_name]
+          group node[:owner_name]
           mode 0644
           variables({
             :app_name => app_name,
-            :user => @node[:owner_name],
+            :user => node[:owner_name],
             :flavor => flavor
           })
         end
 
         template "/data/#{app_name}/shared/config/sphinx.yml" do
-          owner @node[:owner_name]
-          group @node[:owner_name]
+          owner node[:owner_name]
+          group node[:owner_name]
           mode 0644
           source "sphinx.yml.erb"
           variables({
             :app_name => app_name,
             :address => sphinx_host,
-            :user => @node[:owner_name],
+            :user => node[:owner_name],
             :mem_limit => '32M'
           })
         end
@@ -131,10 +131,10 @@ else
 
         execute "sphinx config" do
           command "bundle exec rake #{flavor}:configure"
-          user @node[:owner_name]
+          user node[:owner_name]
           environment({
-            'HOME' => "/home/#{@node[:owner_name]}",
-            'RAILS_ENV' => @node[:environment][:framework_env]
+            'HOME' => "/home/#{node[:owner_name]}",
+            'RAILS_ENV' => node[:environment][:framework_env]
           })
           cwd "/data/#{app_name}/current"
         end
@@ -145,10 +145,10 @@ else
 
         execute "#{flavor} index" do
           command "bundle exec rake #{flavor}:index"
-          user @node[:owner_name]
+          user node[:owner_name]
           environment({
-            'HOME' => "/home/#{@node[:owner_name]}",
-            'RAILS_ENV' => @node[:environment][:framework_env]
+            'HOME' => "/home/#{node[:owner_name]}",
+            'RAILS_ENV' => node[:environment][:framework_env]
           })
           cwd "/data/#{app_name}/current"
         end
@@ -163,36 +163,36 @@ else
             day     '*'
             month   '*'
             weekday '*'
-            command "cd /data/#{app_name}/current && RAILS_ENV=#{@node[:environment][:framework_env]} bundle exec rake #{flavor}:index"
-            user @node[:owner_name]
+            command "cd /data/#{app_name}/current && RAILS_ENV=#{node[:environment][:framework_env]} bundle exec rake #{flavor}:index"
+            user node[:owner_name]
           end
         end
       end
     end
   else
-    if ['solo', 'app', 'app_master'].include?(@node[:instance_role])
+    if ['solo', 'app', 'app_master'].include?(node[:instance_role])
       run_for_app(appname) do |app_name, data|
         ey_cloud_report "Sphinx" do
           message "configuring #{flavor}"
         end
 
         directory "/var/run/sphinx" do
-          owner @node[:owner_name]
-          group @node[:owner_name]
+          owner node[:owner_name]
+          group node[:owner_name]
           mode 0755
         end
 
         directory "/var/log/engineyard/sphinx/#{app_name}" do
           recursive true
-          owner @node[:owner_name]
-          group @node[:owner_name]
+          owner node[:owner_name]
+          group node[:owner_name]
           mode 0755
         end
 
         directory "/data/#{app_name}/shared/config/sphinx" do
           recursive true
-          owner @node[:owner_name]
-          group @node[:owner_name]
+          owner node[:owner_name]
+          group node[:owner_name]
           mode 0755
         end
 
@@ -207,26 +207,26 @@ else
 
         template "/etc/monit.d/sphinx.#{app_name}.monitrc" do
           source "sphinx.monitrc.erb"
-          owner @node[:owner_name]
-          group @node[:owner_name]
+          owner node[:owner_name]
+          group node[:owner_name]
           mode 0644
           variables({
             :app_name => app_name,
-            :user => @node[:owner_name],
-            :env => @node[:environment][:framework_env],
+            :user => node[:owner_name],
+            :env => node[:environment][:framework_env],
             :flavor => flavor
           })
         end
 
         template "/data/#{app_name}/shared/config/sphinx.yml" do
-          owner @node[:owner_name]
-          group @node[:owner_name]
+          owner node[:owner_name]
+          group node[:owner_name]
           mode 0644
           source "sphinx.yml.erb"
           variables({
             :app_name => app_name,
             :address => 'localhost',
-            :user => @node[:owner_name],
+            :user => node[:owner_name],
             :mem_limit => '32'
           })
         end
@@ -240,10 +240,10 @@ else
 
         execute "sphinx config" do
           command "bundle exec rake #{flavor}:configure"
-          user @node[:owner_name]
+          user node[:owner_name]
           environment({
-            'HOME' => "/home/#{@node[:owner_name]}",
-            'RAILS_ENV' => @node[:environment][:framework_env]
+            'HOME' => "/home/#{node[:owner_name]}",
+            'RAILS_ENV' => node[:environment][:framework_env]
           })
           cwd "/data/#{app_name}/current"
         end
@@ -254,10 +254,10 @@ else
 
         execute "#{flavor} index" do
           command "bundle exec rake #{flavor}:index"
-          user @node[:owner_name]
+          user node[:owner_name]
           environment({
-            'HOME' => "/home/#{@node[:owner_name]}",
-            'RAILS_ENV' => @node[:environment][:framework_env]
+            'HOME' => "/home/#{node[:owner_name]}",
+            'RAILS_ENV' => node[:environment][:framework_env]
           })
           cwd "/data/#{app_name}/current"
         end
@@ -272,8 +272,8 @@ else
             day     '*'
             month   '*'
             weekday '*'
-            command "cd /data/#{app_name}/current && RAILS_ENV=#{@node[:environment][:framework_env]} bundle exec rake #{flavor}:index"
-            user @node[:owner_name]
+            command "cd /data/#{app_name}/current && RAILS_ENV=#{node[:environment][:framework_env]} bundle exec rake #{flavor}:index"
+            user node[:owner_name]
           end
         end
       end
